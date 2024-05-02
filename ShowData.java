c8d43fb28761627208bba64f9193cd5a4743f008
Samundeeswari Velusamy <118312757+Samunde@users.noreply.github.com>
2024-05-02 18:29:18 -0400

    Add files via upload
    Added files

---
+++ ShowData.java
@@ -0,0 +1,18 @@
+package com.sam.ArrayListOfUserDefined_objects;
+
+import java.util.ArrayList;
+
+public class ShowData {
+    public static void main(String[] args) {
+        //Instantiation to class AddDataToArrayList
+        AddDataToArrayList b = new AddDataToArrayList();
+        ArrayList<Book> mybooklist = b.bookdetails();
+        for(Book showValue: mybooklist)
+        {
+            // ---- invoking getter method for geting Data---------
+            System.out.println(showValue.getNumber() + " " + showValue.getName() +" "+ showValue.getCategory() +" "+ showValue.getAuthor());
+        }
+    }
+}
+
+
